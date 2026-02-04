d7d9a0bf0a159d3b3460f8daaf95f3e389959bfe
Shola <sholagulmaliyeva@gmail.com>
2026-02-04 07:00:47 +0000

    editing sort.h

--- sort.h
+++ sort.h
@@ -3,12 +3,26 @@
 
 #include <stddef.h>
 
-/* Prototype for printing an array of integers */
-void print_array(const int *array, size_t size);
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
-/* Prototype for Bubble Sort */
+/* Print a doubly linked list of integers */
+void print_list(const listint_t *list);
+
+/* Bubble sort for an array of integers */
 void bubble_sort(int *array, size_t size);
 
-/*insertion sort for a doubly linked list*/
+/* Insertion sort for a doubly linked list */
 void insertion_sort_list(listint_t **list);
+
 #endif /* SORT_H */
